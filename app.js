c49b6791086f3c7e01e34b5ad379a1dc7da24fc3
sachin-yadav-1 <sachin.y4dav@gmail.com>
2020-12-24 14:46:59 +0530

    Security Features Implemented

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController.js');
 
@@ -9,8 +14,48 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 // APP SETUP
 const app = express();
-app.use(express.json());
 dotenv.config();
+app.use(express.json({ limit: '10kb' })); // Will not process any request above '10kb' size
+
+// DATA SANITIZATION: NoSQL QUER INJECTIONS
+// Removes all the '$' signs and other operators from request body and params
+app.use(mongoSanitize());
+
+// DATA SANITIZATION: XSS ATTACKS
+// Removes malicious HTML or any other code by removing HTML tags to HTML symbols
+app.use(xss());
+
+// SECURITY HTTP HEADERS
+app.use(helmet());
+
+// RATE LIMITER
+// Limits the number of requests in a given time frame ( in this case: 1hr )
+const limiter = rateLimit({
+  max: 50,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests, please try again in an hour.',
+});
+
+// Limit requests only to 'api' routes
+app.use('/api', limiter);
+
+// PREVENT PARAMETER POLLUTION
+// Removes duplicate parameters from the request
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+// SERVE STATIC FILES
+// Server static files from 'public' folder
+app.use(express.static(`${__dirname}/public`));
 
 // MOUNTING ROUTES
 app.use('/api/tours', tourRouter);
