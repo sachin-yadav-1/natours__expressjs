85b73f013754922543a30314e76a7035ef162686
sachin-yadav-1 <sachin.y4dav@gmail.com>
2020-12-24 15:41:24 +0530

    Exception and Rejection Error Handlers

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
+const dotenv = require('dotenv');
+
+// GLOBAL UNHANDLED EXCEPTION ( ASYNC CODE ERRORS ) HANDLER
+process.on('unhandledException', (err) => {
+  console.log('UNHANDLED EXCEPTION');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 // DATABASE SETUP
 const DB = process.env.DB_LOCAL;
@@ -10,12 +18,21 @@ mongoose
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('DB Connection Successful!'))
-  .catch((err) => {
-    console.log(err.message);
-    process.exit(1);
-  });
+  .then(() => console.log('DB Connection Successful!'));
 
 // SERVER SETUP
 const port = process.env.PORT;
-app.listen(port, () => console.log(`Listening to server at port: ${port}`));
+const server = app.listen(port, () =>
+  console.log(`Listening to server at port: ${port}`)
+);
+
+// GLOBAL UNHANDLED REJECTION ( PROMISE ) HANDLER
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION');
+  console.log(err.name, err.message);
+
+  // Close server and exit
+  server.close(() => {
+    process.exit(1);
+  });
+});
